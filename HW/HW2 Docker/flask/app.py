2e298d9c4f044cb4da2be8df0f712412f59c5004
lindsayjthurston <ljt@bu.edu>
2025-02-09 15:11:46 -0500

    Initial commit: Flask app with /repeat, /health, and environment-based port

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, make_response
+import os
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
@@ -12,8 +13,31 @@ def hello():
     )
     return response
 
+@app.route('/repeat', methods=['GET'])
+def repeat():
+    user_input = request.args.get("input", "default")
+    response = make_response(
+	{
+	    "body": user_input,
+	    "status": 200
+	}
+    )
+    return response
+
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    response = make_response(
+	{
+	    "body": "OK",
+	    "status": 200
+	}
+    )
+    return response
+
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.getenv("PORT", 5002))
+    app.run(host='0.0.0.0',port=port, debug=False)
